1a3ed8c0478c770d5280aae3bbd994560ecd4ce0
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-09-02 12:57:22 +0100

    update: check for existing project, store project title in lowercase, auth middleware in routes

--- projects/projects.routes.js
+++ projects/projects.routes.js
@@ -6,10 +6,15 @@ import {
   deleteProject,
   updateProject,
 } from "./projects.controllers.js";
+import {
+  verifyAdmin,
+  verifyUser,
+  verifyToken,
+} from "../middleware/authenticate.js";
 
 const projectRouter = express.Router();
 
-projectRouter.post("/", createProject);
+projectRouter.post("/", verifyToken, createProject);
 projectRouter.get("/", getAllProjects);
 projectRouter.get("/:id", getProjectById);
 projectRouter.patch("/:id", updateProject);
