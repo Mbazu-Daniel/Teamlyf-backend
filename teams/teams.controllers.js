5aaaff6f5ff22d0367669f6c6590ee640144d17e
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-09-05 13:49:08 +0100

    fix: refactor teams CRUD endpoints to work with organization and employee

--- teams/teams.controllers.js
+++ teams/teams.controllers.js
@@ -1,10 +1,27 @@
 import asyncHandler from "express-async-handler";
 import Team from "./teams.models.js";
+import Organization from "../organizations/organizations.models.js";
 
-// Create a new team
+// Create a new team within an organization
 const createTeam = asyncHandler(async (req, res) => {
   try {
-    const newTeam = await Team.create(req.body);
+    const { organizationId } = req.params;
+    const { createdBy } = req.user.id;
+    const organization = await Organization.findById(organizationId);
+
+    if (!organization) {
+      return res.status(404).json({ error: "Organization not found" });
+    }
+
+    const newTeam = await Team.create({
+      ...req.body,
+      createdBy: createdBy,
+      organization: organizationId,
+    });
+
+    // Add the team to the organization's list of teams
+    organization.teams.push(newTeam._id);
+    await organization.save();
 
     res.status(201).json(newTeam);
   } catch (error) {
@@ -12,24 +29,29 @@ const createTeam = asyncHandler(async (req, res) => {
   }
 });
 
-// Get all teams
+// Get all teams within an organization
 const getAllTeams = asyncHandler(async (req, res) => {
   try {
-    // const teams = await Team.find();
-    res.status(200).json(await Team.find());
+    const { organizationId } = req.params;
+    const teams = await Team.find({ organization: organizationId });
+
+    res.status(200).json(teams);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Get a team by ID
+// Get a team by ID within an organization
 const getTeamById = asyncHandler(async (req, res) => {
-  const { id } = req.params;
+  const { organizationId, teamId } = req.params;
 
   try {
-    const team = await Team.findById(id);
+    const team = await Team.findOne({
+      _id: teamId,
+      organization: organizationId,
+    });
     if (!team) {
-      res.status(404).json(`Team ${id} not found`);
+      res.status(404).json(`Team ${teamId} not found`);
     }
     res.status(200).json(team);
   } catch (error) {
@@ -37,34 +59,49 @@ const getTeamById = asyncHandler(async (req, res) => {
   }
 });
 
-// Delete a team by ID
+// Delete a team by ID within an organization
 const deleteTeam = asyncHandler(async (req, res) => {
-  const { id } = req.params;
+  const { organizationId, teamId } = req.params;
 
   try {
-    const team = await Team.findById(id);
+    const team = await Team.findOne({
+      _id: teamId,
+      organization: organizationId,
+    });
     if (!team) {
-      res.status(404).json(`Team ${id} not found`);
+      res.status(404).json(`Team ${teamId} not found`);
     }
-    await Team.findByIdAndDelete(id);
-    res.status(204).json(`Team ${id} deleted`);
+
+    await Team.findByIdAndDelete(teamId);
+
+    // Remove the team ID from the organization's list of teams
+    const organization = await Organization.findById(organizationId);
+    organization.teams.pull(teamId);
+    await organization.save();
+
+    res.status(204).json(`Team ${teamId} deleted`);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Update a team by ID
+// Update a team by ID within an organization
 const updateTeam = asyncHandler(async (req, res) => {
-  const { id } = req.params;
+  const { organizationId, teamId } = req.params;
 
   try {
-    const team = await Team.findById(id);
+    const team = await Team.findOne({
+      _id: teamId,
+      organization: organizationId,
+    });
     if (!team) {
-      res.status(404).json(`Team ${id} not found`);
+      res.status(404).json(`Team ${teamId} not found`);
     }
-    const updatedTeam = await Team.findByIdAndUpdate(id, req.body, {
+
+    const updatedTeam = await Team.findByIdAndUpdate(teamId, req.body, {
       new: true,
     });
+
     res.status(202).json(updatedTeam);
   } catch (error) {
     res.status(500).json({ error: error.message });
