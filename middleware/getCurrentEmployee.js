08a6205266549b463be0c62785a9a2b7f1b257e5
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-10-19 20:53:59 +0100

    feat: created getCurrentWorkspace and getCurrentEmployee middleware

---
+++ middleware/getCurrentEmployee.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import asyncHandler from "express-async-handler";
+
+const prisma = new PrismaClient();
+
+export const getCurrentEmployee = asyncHandler(async (req, res, next) => {
+  const { id: userId } = req.user;
+  const { workspaceId } = req.params;
+
+  try {
+    // Query the database to check if the user has an associated employee within the workspace
+    const employee = await prisma.employee.findFirst({
+      where: {
+        userId, // Assuming user ID is available on the user object
+        workspaceId, // You may access the workspace ID from the request
+      },
+    });
+    if (!employee) {
+      return res
+        .status(403)
+        .json({ error: "User is not an employee in this workspace" });
+    }
+    // Attach the employee object to the request for further use if needed
+    req.employeeId = employee.id;
+
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
