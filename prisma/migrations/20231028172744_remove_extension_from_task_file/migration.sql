6157e46db5a99d7d86a6e272c48bf720556e03f8
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-10-28 22:00:15 +0100

    fix: removed extension from taskfile model

---
+++ prisma/migrations/20231028172744_remove_extension_from_task_file/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `extension` on the `TaskFile` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "TaskFile" DROP COLUMN "extension";
