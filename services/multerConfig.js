390dbbad81a49cc51e6eb451a6a1af96f97c4735
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-10-28 21:58:44 +0100

    feat: created multer service to handle file upload handling

---
+++ services/multerConfig.js
@@ -0,0 +1,17 @@
+import multer, { diskStorage } from 'multer';
+
+// Define storage and file naming
+const storage = diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Set the directory where files will be stored
+  },
+  filename: (req, file, cb) => {
+    // Rename the file to avoid conflicts, e.g., using a timestamp
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1]);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export default upload;
