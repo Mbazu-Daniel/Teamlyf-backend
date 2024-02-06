3d1dc97cb8c7d3499f99ee803d14b6587dd471b5
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-02-06 16:50:34 +0100

    feat: updated swagger ui with backend servers

--- swagger-autogen.js
+++ swagger-autogen.js
@@ -1,5 +1,6 @@
 import swaggerAutogen from 'swagger-autogen';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const outputFile = './swagger.json';
 const endpointsFiles = ['./*/*/*.routes.js'];
 
@@ -10,6 +11,21 @@ const config = {
 			'Welcome to TeamLyf, the ultimate solution for seamless collaboration and enhance teamwork within your organization. Our platform integrates essential tools including HR Management, Chat Application, Video Conferencing, Task/Project Management, and Document Management System into a single, user-friendly interface. TeamLyf provides an integrated solution to foster collaboration and productivity.',
 		version: '1.0.0',
 	},
+
+	servers: [
+		{
+			url: 'http://localhost:8000/api/v1',
+			description: 'local testing server',
+		},
+		{
+			url: 'https://teamlyf.cyclic.app/api/v1',
+			description: 'cyclic testing server',
+		},
+		{
+			url: 'https://teamlyf.onrender.com/api/v1',
+			description: 'render testing server',
+		},
+	],
 	tags: [
 		'Auth',
 		'Users',
@@ -22,7 +38,7 @@ const config = {
 		'Leave Comment',
 		'Leave Type',
 	],
-	host: 'localhost:8000/api/v1' || process.env.API_URL,
+	// host: 'localhost:8000/api/v1' || process.env.API_URL,
 
 	components: {
 		securitySchemes: {
