bd213338ecb9e9e7f390f3703d87222007be2b95
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-02-15 22:14:11 +0100

    fix: create custom schema for project

--- swagger-autogen.js
+++ swagger-autogen.js
@@ -1,61 +1,79 @@
-import swaggerAutogen from 'swagger-autogen';
-import dotenv from 'dotenv';
+import swaggerAutogen from "swagger-autogen";
+import dotenv from "dotenv";
 dotenv.config();
-const outputFile = './swagger.json';
-const endpointsFiles = ['./*/*/*.routes.js'];
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./*/*/*.routes.js"];
 
 const config = {
-	info: {
-		title: 'Teamlyf API Documentation',
-		description:
-			'Welcome to TeamLyf, the ultimate solution for seamless collaboration and enhance teamwork within your organization. Our platform integrates essential tools including HR Management, Chat Application, Video Conferencing, Task/Project Management, and Document Management System into a single, user-friendly interface. TeamLyf provides an integrated solution to foster collaboration and productivity.',
-		version: '1.0.0',
-	},
+  info: {
+    title: "Teamlyf API Documentation",
+    description:
+      "Welcome to TeamLyf, the ultimate solution for seamless collaboration and enhance teamwork within your organization. Our platform integrates essential tools including HR Management, Chat Application, Video Conferencing, Task/Project Management, and Document Management System into a single, user-friendly interface. TeamLyf provides an integrated solution to foster collaboration and productivity.",
+    version: "1.0.0",
+  },
 
-	servers: [
-		{
-			url: 'http://localhost:8000/api/v1',
-			description: 'local testing server',
-		},
-		{
-			url: 'https://teamlyf.cyclic.app/api/v1',
-			description: 'cyclic testing server',
-		},
-		{
-			url: 'https://teamlyf.onrender.com/api/v1',
-			description: 'render testing server',
-		},
-	],
-	tags: [
-		'Auth',
-		'Users',
-		'Workspace',
-		'Invite',
-		'Employees',
-		'Teams',
-		'Project',
-		'Leave',
-		'Leave Comment',
-		'Leave Type',
-	],
-	// host: 'localhost:8000/api/v1' || process.env.API_URL,
+  servers: [
+    {
+      url: "http://localhost:8000/api/v1",
+      description: "local testing server",
+    },
+    {
+      url: "https://teamlyf.cyclic.app/api/v1",
+      description: "cyclic testing server",
+    },
+    {
+      url: "https://teamlyf.onrender.com/api/v1",
+      description: "render testing server",
+    },
+  ],
+  tags: [
+    "Auth",
+    "Users",
+    "Workspace",
+    "Invite",
+    "Employees",
+    "Teams",
+    "Project",
+    "Leave",
+    "Leave Comment",
+    "Leave Type",
+  ],
 
-	components: {
-		securitySchemes: {
-			bearerAuth: {
-				type: 'http',
-				scheme: 'bearer',
-			},
-		},
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
 
-		schemas: {
-			LoginRequest: {
-				$email: 'example@gmail.com',
-				$password: 'password',
-			},
-		},
-	},
-	schemes: ['http', 'https'],
+    schemas: {
+      LoginRequest: {
+        $email: "example@gmail.com",
+        $password: "password",
+      },
+
+      UpdateProjectDetails: {
+        $name: "string",
+        $description: "string",
+        $thumbnail: "string",
+        $category: "string",
+        $startDate: "string",
+        $dueDate: "string",
+        $collaboratorIds: "string",
+        $customTaskStatusId: "string",
+        $projectPriorityId: "string",
+      },
+
+      RemoveProjectCollaborators: {
+        $employeeIds: "string",
+      },
+      RemoveGroupCollaborators: {
+        $memberIds: "string",
+      },
+    },
+  },
+  schemes: ["http", "https"],
 };
 
-swaggerAutogen({ openapi: '3.0.0' })(outputFile, endpointsFiles, config);
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, config);
