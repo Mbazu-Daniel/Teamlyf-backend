beac9706d0d0a2e54687a3512db043096ca23dc4
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-05-18 09:13:28 +0100

    feat: set up  web socket server, rate limiter and cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import app from './app.js';
+import { httpServer } from "./app.js";
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-	console.log(`Server is running on PORT ${PORT}`);
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
