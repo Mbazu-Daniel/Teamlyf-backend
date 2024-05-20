bbc65e53b4e8a9c78b04d62fef9dce5f55dc8a89
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-05-20 07:39:44 +0100

    feat: send messages or files to user and also emit event

--- chats/messages/messages.routes.js
+++ chats/messages/messages.routes.js
@@ -1,40 +1,33 @@
-import express from 'express';
-import { verifyToken } from '../../utils/middleware/authenticate.js';
+import express from "express";
+import { verifyToken } from "../../utils/middleware/authenticate.js";
 import {
-	getCurrentEmployee,
-	getCurrentWorkspace,
-} from '../../utils/middleware/index.js';
+  getCurrentEmployee,
+  getCurrentWorkspace,
+} from "../../utils/middleware/index.js";
 
 import {
-	getConversationMessages,
-	sendConversationMessage,
-	deleteConversationMessage,
-} from './messages.controllers.js';
+  // getConversationMessages,
+  sendDirectMessage,
+
+} from "./messages.controllers.js";
 
 const messageRouter = express.Router();
 const app = express();
 
 app.use(
-	'/workspace',
-	messageRouter
-	/* 
+  "/:workspaceId",
+  messageRouter
+  /* 
   
   #swagger.tags = ['Messages']
   */
 );
 messageRouter.use(verifyToken, getCurrentEmployee, getCurrentWorkspace);
 
-messageRouter.get(
-	'/:workspaceId/conversations/:conversationId/messages',
-	getConversationMessages
-);
 messageRouter.post(
-	'/:workspaceId/conversations/:conversationId/messages/send',
-	sendConversationMessage
+  "/:workspaceId/conversations/:conversationId/messages/send",
+  sendDirectMessage
 );
-messageRouter.delete(
-	'/:workspaceId/conversations/:conversationId/messages/:messageId/delete',
-	deleteConversationMessage
-);
+
 
 export default messageRouter;
